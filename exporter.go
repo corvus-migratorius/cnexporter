f81e78e6ee9f8ddc2046818828bc2ab39dc09c74
Alexander Gorelyshev <alexander.gorelyshev@pm.me>
2023-09-12 15:32:28 +0400

    Initial commit

---
+++ exporter.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+    containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+            fmt.Printf(
+				"%s : %s : %s : %s : %s\n",
+				container.ID[:10],
+				container.Image,
+				container.Names[0],
+				container.Status,
+				container.State,
+			)
+	}
+}
