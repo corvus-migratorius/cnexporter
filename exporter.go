46234d8b6feaa1d900c331e1ffbad400f9a32bd9
Alexander Gorelyshev <alexander.gorelyshev@pm.me>
2023-09-12 15:57:32 +0400

    Add a toy metric

--- exporter.go
+++ exporter.go
@@ -4,11 +4,30 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	cntStats = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "test_stat",
+		Help: "This is a test Gauge metric",
+	})
+)
+
+func recordMetrics() {
+	go func() {
+		cntStats.Set(0)
+		for {
+			time.Sleep(15 * time.Second)
+		}
+	}()
+}
 
 func main() {
 	ctx := context.Background()
@@ -18,22 +37,24 @@ func main() {
 	}
 	defer cli.Close()
 
-    containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
 	if err != nil {
 		panic(err)
 	}
 
 	for _, container := range containers {
-            fmt.Printf(
-				"%s : %s : %s : %s : %s\n",
-				container.ID[:10],
-				container.Image,
-				container.Names[0],
-				container.Status,
-				container.State,
-			)
+		fmt.Printf(
+			"%s : %s : %s : %s : %s\n",
+			container.ID[:10],
+			container.Image,
+			container.Names[0],
+			container.Status,
+			container.State,
+		)
 	}
 
+	recordMetrics()  // a coroutine
+
 	http.Handle("/metrics", promhttp.Handler())
-    http.ListenAndServe(":2112", nil)
+	http.ListenAndServe(":2112", nil)
 }
