307dcca7a42544ced9b8b01edf2503d76d3647a5
Alexander Gorelyshev <alexander.gorelyshev@pm.me>
2023-09-14 17:25:06 +0400

    Factor out the coroutines into the "library" module

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -1,9 +1,15 @@
 package exporter
 
 import (
+	"context"
+	"log"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type CntLabels struct {
@@ -12,6 +18,13 @@ type CntLabels struct {
 	Name   string
 	Status string
 	State  string
+}
+
+type CntCounts struct {
+	Total   *prometheus.GaugeVec
+	Created *prometheus.GaugeVec
+	Running *prometheus.GaugeVec
+	Exited  *prometheus.GaugeVec
 }
 
 func CountByStatus(containers []types.Container, state string) int {
@@ -32,4 +45,63 @@ func BuildLabels(container types.Container) CntLabels {
 		container.Status,
 		container.State,
 	}
+}
+
+func RecordCounts(counts CntCounts, dclient *client.Client, dcontext context.Context) {
+	go func() {
+		for {
+			containers, err := dclient.ContainerList(dcontext, types.ContainerListOptions{All: true})
+			if err != nil {
+				panic(err)
+			}
+
+			hostname, err := os.Hostname()
+			if err != nil {
+				log.Fatal("Failed to read the hostname")
+			}
+
+			cnt_total := float64(len(containers))
+			cnt_created := float64(CountByStatus(containers, "created"))
+			cnt_running := float64(CountByStatus(containers, "running"))
+			cnt_exited := float64(CountByStatus(containers, "exited"))
+
+			counts.Total.With(prometheus.Labels{"nodename": hostname}).Set(cnt_total)
+			counts.Created.With(prometheus.Labels{"nodename": hostname}).Set(cnt_created)
+			counts.Running.With(prometheus.Labels{"nodename": hostname}).Set(cnt_running)
+			counts.Exited.With(prometheus.Labels{"nodename": hostname}).Set(cnt_exited)
+
+			time.Sleep(15 * time.Second)
+		}
+	}()
+}
+
+func RecordMetadata(metadata *prometheus.GaugeVec, dclient *client.Client, dcontext context.Context) {
+	go func() {
+		for {
+			containers, err := dclient.ContainerList(dcontext, types.ContainerListOptions{All: true})
+			if err != nil {
+				panic(err)
+			}
+
+			hostname, err := os.Hostname()
+			if err != nil {
+				log.Fatal("Failed to read the hostname")
+			}
+
+			metadata.Reset()
+			for _, cnt := range containers {
+				labels := BuildLabels(cnt)
+				metadata.With(prometheus.Labels{
+					"id":       labels.Id,
+					"image":    labels.Image,
+					"name":     labels.Name,
+					"status":   labels.Status,
+					"state":    labels.State,
+					"nodename": hostname,
+				})
+			}
+
+			time.Sleep(15 * time.Second)
+		}
+	}()
 }
--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -15,80 +12,6 @@ import (
 
 	"docker-exporter/exporter"
 )
-
-var (
-	cntStats = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "migratorius_docker_cnt_metadata",
-		Help: "Container metadata",
-	},
-		[]string{"id", "image", "name", "status", "state", "nodename"},
-	)
-	cntCountTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "migratorius_docker_cnt_counts",
-		Help: "Number of Docker containers detected on the node",
-	},
-		[]string{"nodename"},
-	)
-	cntCountCreated = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "migratorius_docker_cnt_created",
-		Help: "Number of Docker containers with status 'created'",
-	},
-		[]string{"nodename"},
-	)
-	cntCountRunning = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "migratorius_docker_cnt_running",
-		Help: "Number of Docker containers with status 'running'",
-	},
-		[]string{"nodename"},
-	)
-	cntCountExited = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "migratorius_docker_cnt_exited",
-		Help: "Number of Docker containers with status 'exited'",
-	},
-		[]string{"nodename"},
-	)
-)
-
-func recordMetrics(dclient *client.Client, dcontext context.Context) {
-	go func() {
-		for {
-			containers, err := dclient.ContainerList(dcontext, types.ContainerListOptions{All: true})
-			if err != nil {
-				panic(err)
-			}
-
-			hostname, err := os.Hostname()
-			if err != nil {
-				log.Fatal("Failed to read the hostname")
-			}
-
-			cnt_total := float64(len(containers))
-			cnt_created := float64(exporter.CountByStatus(containers, "created"))
-			cnt_running := float64(exporter.CountByStatus(containers, "running"))
-			cnt_exited := float64(exporter.CountByStatus(containers, "exited"))
-
-			cntStats.Reset()
-			for _, cnt := range containers {
-				labels := exporter.BuildLabels(cnt)
-				cntStats.With(prometheus.Labels{
-					"id":       labels.Id,
-					"image":    labels.Image,
-					"name":     labels.Name,
-					"status":   labels.Status,
-					"state":    labels.State,
-					"nodename": hostname,
-				})
-			}
-
-			cntCountTotal.With(prometheus.Labels{"nodename": hostname}).Set(cnt_total)
-			cntCountCreated.With(prometheus.Labels{"nodename": hostname}).Set(cnt_created)
-			cntCountRunning.With(prometheus.Labels{"nodename": hostname}).Set(cnt_running)
-			cntCountExited.With(prometheus.Labels{"nodename": hostname}).Set(cnt_exited)
-
-			time.Sleep(15 * time.Second)
-		}
-	}()
-}
 
 func main() {
 	dcontext := context.Background()
@@ -98,7 +21,41 @@ func main() {
 	}
 	defer dclient.Close()
 
-	recordMetrics(dclient, dcontext) // a coroutine
+	cntMetadata := promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "migratorius_docker_cnt_metadata",
+		Help: "Container metadata",
+	},
+		[]string{"id", "image", "name", "status", "state", "nodename"},
+	)
+
+	var cntCounts exporter.CntCounts
+	cntCounts.Total = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "migratorius_docker_cnt_counts",
+		Help: "Number of Docker containers detected on the node",
+	},
+		[]string{"nodename"},
+	)
+	cntCounts.Created = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "migratorius_docker_cnt_created",
+		Help: "Number of Docker containers with status 'created'",
+	},
+		[]string{"nodename"},
+	)
+	cntCounts.Running = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "migratorius_docker_cnt_running",
+		Help: "Number of Docker containers with status 'running'",
+	},
+		[]string{"nodename"},
+	)
+	cntCounts.Exited = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "migratorius_docker_cnt_exited",
+		Help: "Number of Docker containers with status 'exited'",
+	},
+		[]string{"nodename"},
+	)
+
+	exporter.RecordCounts(cntCounts, dclient, dcontext)
+	exporter.RecordMetadata(cntMetadata, dclient, dcontext)
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":2112", nil))
